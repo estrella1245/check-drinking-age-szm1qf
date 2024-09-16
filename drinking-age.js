87b31e3948479f05a7aee0b2162635fade0207c7
estrella1245 <emonroy13@student.cccd.edu>
2024-09-15 21:53:45 -0700

    made code

--- drinking-age.js
+++ drinking-age.js
@@ -10,7 +10,9 @@ console.log('age is ' + age);
 
 // TODO: Checks the given age to see whether it's legal to drink in the US or not.
 // Set to true if the age is 21 years or older. Otherwise, set to false.
-const result = undefined;
+let result = undefined;
+
+(parseInt(age) >= 21) ? result = true : result = false;
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE.
 console.log('result= ' + result);
